1254e3cb3cc890566bd1057d4aa4f30548179043
Nikhil Garg <116938919+nikhilgarg26@users.noreply.github.com>
2024-01-18 23:39:58 +0530

    env files added

--- server/index.js
+++ server/index.js
@@ -2,16 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
 const userRoutes = require('./routes/user')
+require('dotenv').config()
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 // Middlewares
 app.use(cors())
 app.use(express.json())
 
 // MongoConnection
-mongoose.connect('mongodb+srv://nikhilgarg26:z0AVr6QhozwszKMG@cluster0.6wcwk7j.mongodb.net/?retryWrites=true&w=majority', { dbName: 'otpverify', }).then(()=>{console.log('Connected to DB')})
+mongoose.connect(process.env.MONGOURL, { dbName: 'otpverify', }).then(() => { console.log('Connected to DB') })
 
 // Routes
 app.use('/auth', userRoutes)
